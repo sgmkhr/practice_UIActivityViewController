d5b08f5cf447e1d538658e62506e540d63cae79f
sgmkhr <mksghr10@gmail.com>
2024-09-13 06:55:47 +0900

    Initial Commit

---
+++ practice_UIActivityViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice_UIActivityViewController
+//
+//  Created by 菅原実希 on 2024/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
